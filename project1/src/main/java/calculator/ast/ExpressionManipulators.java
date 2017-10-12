7b6454232b52a3dc9342856104e9d0728f05237c
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2017-10-11 17:19:25 -0700

    Add comments

--- project1/src/main/java/calculator/ast/ExpressionManipulators.java
+++ project1/src/main/java/calculator/ast/ExpressionManipulators.java
@@ -1,3 +1,9 @@
+/*
+ * Team Members
+ * Harshitha Akkaraju
+ * Shaarika Kaul
+ */
+
 package calculator.ast;
 import calculator.interpreter.Environment;
 import calculator.errors.EvaluationError;
@@ -26,14 +32,18 @@ public class ExpressionManipulators {
      * @throws EvaluationError  if any of the expressions uses an unknown operation.
      */
     public static AstNode toDouble(Environment env, AstNode node) {
-        // To help you get started, we've implemented this method for you.
-        // You should fill in the TODOs in the 'toDoubleHelper' method.
+    		// to avoid the extraneous toDouble() string
     		if (node.isOperation() && node.getName().equals("toDouble")) {
     			return new AstNode(toDoubleHelper(env.getVariables(), node.getChildren().get(0)));
 		}
         return new AstNode(toDoubleHelper(env.getVariables(), node));
     }
 
+    /*
+     * Takes a node of type either operation, variable, or number, and a dictionary of variables
+     * Evaluates the given AST node tree and returns a double
+     * throws EvaluationError exception if there is an undefined variable
+     */
     private static double toDoubleHelper(IDictionary<String, AstNode> variables, AstNode node) {
         // There are three types of nodes, so we have three cases.
         if (node.isNumber()) {
@@ -75,19 +85,17 @@ public class ExpressionManipulators {
     }
 
     public static AstNode simplify(Environment env, AstNode node) {
-        // Try writing this one on your own!
-        // Hint 1: Your code will likely be structured roughly similarly
-        //         to your "toDouble" method
-        // Hint 2: When you're implementing constant folding, you may want
-        //         to call your "toDouble" method in some way
-
-        // TODO: Your code here
+    		// to avoid the extraneous simplify() string
     		if (node.isOperation() && node.getName().equals("simplify")) {
     			return simplifyHelper(env.getVariables(), node.getChildren().get(0));
     		}
     		return simplifyHelper(env.getVariables(), node);
     }
     
+    /*
+     * Takes an AstNode of type either variable, number, or operation and simplifies it
+     * Returns a node that's been simplified to the lowest level, leaving placeholder variables
+     */
     private static AstNode simplifyHelper(IDictionary<String, AstNode> variables, AstNode node) {
     		if (node.isNumber()) {
     			return node;
@@ -95,90 +103,78 @@ public class ExpressionManipulators {
     			if (variables.containsKey(node.getName())) {
     				return variables.get(node.getName());
     			}
-    			return node;
+    			return node; // already in it's simplest form
     		} else {
     			String name = node.getName();
     			IList<AstNode> nodes = node.getChildren();
     			if (name.equals("+") || name.equals("-") || name.equals("*") || name.equals("/")) {
+    				// to simplify farther down the tree
     				AstNode child1 = simplifyHelper(variables, nodes.get(0));
     				AstNode child2 = simplifyHelper(variables, nodes.get(1));
+    				// performs the 'operation' if the nodes are of type numeric
     				if (!name.equals("/") && child1.isNumber() && child2.isNumber()) {
     					return new AstNode(toDoubleHelper(variables, node));
     				} else {
     					nodes.set(0, child1);
     					nodes.set(1, child2);
+    					// returns a new node with updated child nodes
     					return new AstNode(name, nodes);
     				}
     			} else {
     				AstNode child = simplifyHelper(variables, nodes.get(0));
     				nodes.set(0, child);
+    				// returns a new node with updated child nodes
     				return new AstNode(name, nodes);
     			}
     		}
 	}
 
-    /**
-     * Expected signature of plot:
-     *
-     * >>> plot(exprToPlot, var, varMin, varMax, step)
-     *
-     * Example 1:
-     *
-     * >>> plot(3 * x, x, 2, 5, 0.5)
-     *
-     * This command will plot the equation "3 * x", varying "x" from 2 to 5 in 0.5
-     * increments. In this case, this means you'll be plotting the following points:
-     *
-     * [(2, 6), (2.5, 7.5), (3, 9), (3.5, 10.5), (4, 12), (4.5, 13.5), (5, 15)]
-     *
-     * ---
-     *
-     * Another example: now, we're plotting the quadratic equation "a^2 + 4a + 4"
-     * from -10 to 10 in 0.01 increments. In this case, "a" is our "x" variable.
-     *
-     * >>> c := 4
-     * 4
-     * >>> step := 0.01
-     * 0.01
-     * >>> plot(a^2 + c*a + a, a, -10, 10, step)
-     *
-     * ---
-     *
-     * @throws EvaluationError  if any of the expressions contains an undefined variable.
-     * @throws EvaluationError  if varMin > varMax
-     * @throws EvaluationError  if 'var' was already defined
-     * @throws EvaluationError  if 'step' is zero or negative
+    /*
+     * Takes a node with plot parameters and env object as parameters
+     * Uses the plot parameters to generate the x-values and y-values for the plot
+     * Plots the function and returns the expression.
+     * 
+     * throws EvaluationError if:
+     * - expressions contain undefined variables
+     * - if the lower bound of the plot is greater than the upper bound
+     * - if var is already defined
+     * - if step (the value to increment by) is <= 0
      */
     public static AstNode plot(Environment env, AstNode node) {
+    		// get the plot parameters
     		IList<AstNode> params = node.getChildren();
+    		// simplify varMin, varMax and step values
     		for (int i = 2; i < params.size(); i++) {
     			AstNode input = params.get(i);
 			input = new AstNode(toDoubleHelper(env.getVariables(), input));
-			params.set(i, input);
+			params.set(i, input); // update
     		}
-    		AstNode exp = params.get(0);
-    		AstNode var = params.get(1);
-    		AstNode min = params.get(2);
-    		AstNode max = params.get(3);
-    		AstNode step = params.get(4);
+    		AstNode exp = params.get(0); // expression
+    		AstNode var = params.get(1); // variable
+    		AstNode min = params.get(2); // min bound for x
+    		AstNode max = params.get(3); // max bound for x
+    		AstNode step = params.get(4); // interval size
     		if (env.getVariables().containsKey(var.getName())
 			||(min.getNumericValue() > max.getNumericValue()) 
 			|| step.getNumericValue() <= 0 
 			|| env.getVariables().containsKey(var.getName())) {
     			throw new EvaluationError("");
     		}
+    		// number of values to generate given the min and the max
     		double numIterations = (max.getNumericValue() - min.getNumericValue()) / step.getNumericValue();
     		IList<Double> xValues = new DoubleLinkedList<Double>();
     		IList<Double> yValues = new DoubleLinkedList<Double>();
     		for (int i = 0; i <= (int) numIterations; i++) {
     			double x = min.getNumericValue() + (i * step.getNumericValue());
     			xValues.add(x);
+    			// populate the variables dictionary
     			env.getVariables().put(var.getName(), new AstNode(x));
     			double y = toDoubleHelper(env.getVariables(), exp);
     			yValues.add(y);	
     		}
-    		env.getVariables().remove(var.getName());
-    		env.getImageDrawer().drawScatterPlot(exp.getName(), "x axis", "y axis", xValues, yValues);
+    		env.getVariables().remove(var.getName()); // reset the variable dictionary
+    		// draw scatter plot
+    		env.getImageDrawer().drawScatterPlot(exp.getName(), var.getName(), exp.getName(), xValues, yValues);
     		return exp;
     }
 }
